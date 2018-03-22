0dcb4f8f5bbf5c802d5566f27d6d5958f7e226a6
swetha1712 <swethakandati@gmail.com>
2018-03-22 13:01:52 -0500

    test

---
+++ src/main/java/DemoGIt/DemoGitTest/App.java
@@ -0,0 +1,13 @@
+package DemoGIt.DemoGitTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
